c284204102458c32544e7363d9a833e6b4d50d36
Jake Morrison <jake@cogini.com>
2017-12-03 09:34:48 +0800

    Allow running on older version of Elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PrometheusExometer.Mixfile do
     [
       app: :prometheus_exometer,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "> 1.4.2",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
