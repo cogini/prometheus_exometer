a0e9dcce0fb9e05997d9b1eb631838fd7cfb0ae9
Jake Morrison <jake@cogini.com>
2024-04-17 15:54:13 -0500

    Bump Elixir/Erlang versions

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule PrometheusExometer.MixProject do
     [
       app: :prometheus_exometer,
       version: @version,
-      elixir: "~> 1.8",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       # build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
