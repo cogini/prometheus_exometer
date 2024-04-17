91555082d2c37b3ca86a581d71058bf8bb631a87
Jake Morrison <jake@cogini.com>
2024-04-17 15:38:59 -0500

    Update libraries and use standard things

--- mix.exs
+++ mix.exs
@@ -2,20 +2,17 @@ defmodule PrometheusExometer.MixProject do
   use Mix.Project
 
   @github "https://github.com/cogini/prometheus_exometer"
+  @version "0.3.0"
 
   def project do
     [
       app: :prometheus_exometer,
-      version: "0.2.0",
+      version: @version,
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
-      build_embedded: Mix.env() == :prod,
+      # build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      description: description(),
-      package: package(),
-      source_url: @github,
-      homepage_url: @github,
-      docs: docs(),
+      aliases: aliases(),
       dialyzer: [
         plt_add_apps: [:mix, :eex]
         # plt_add_deps: true,
@@ -23,18 +20,25 @@ defmodule PrometheusExometer.MixProject do
         # flags: ["-Wunmatched_returns", :error_handling, :race_conditions, :underspecs],
         # ignore_warnings: "dialyzer.ignore-warnings"
       ],
-      deps: deps(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test
-      ]
+        "coveralls.html": :test,
+        "coveralls.lcov": :test,
+        quality: :test,
+        "quality.ci": :test
+      ],
+      description: description(),
+      package: package(),
+      source_url: @github,
+      homepage_url: @github,
+      docs: docs(),
+      deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       # extra_applications: [:logger] ++ extra_applications(Mix.env())
@@ -45,24 +49,25 @@ defmodule PrometheusExometer.MixProject do
   # defp extra_applications(:test), do: []
   # defp extra_applications(_),     do: []
 
-  # Specifies which paths to compile per environment
+  defp elixirc_paths(:dev), do: ["lib", "test/support"]
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.1", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.2", only: [:dev, :test], runtime: false},
       # {:exometer_core, github: "Feuerlabs/exometer_core", tag: "1.5.0"},
-      {:exometer_core, "~> 1.5"},
+      {:exometer_core, "~> 2.0"},
       # {:setup, github: "uwiger/setup", manager: :rebar, override: true}, # fix for https://github.com/uwiger/setup/issues/24
       # https://github.com/Feuerlabs/exometer_core/pull/101
       # https://github.com/uwiger/setup/issues/44
       # {:setup, "~> 2.0", override: true},
-      {:ex_doc, "~> 0.19.2", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.12.0", only: [:dev, :test], runtime: false}
-      # {:mix_test_watch, "~> 0.5", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.30.0", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.18.0", only: [:dev, :test], runtime: false},
+      {:junit_formatter, "~> 3.3", only: [:dev, :test], runtime: false},
+      {:mix_audit, "~> 2.0", only: [:dev, :test], runtime: false}
+      # {:styler, "~> 0.10.0", only: [:dev, :test], runtime: false},
     ]
   end
 
@@ -72,18 +77,59 @@ defmodule PrometheusExometer.MixProject do
 
   defp package do
     [
+      description: description(),
       maintainers: ["Jake Morrison"],
       licenses: ["Mozilla Public License 2.0"],
-      links: %{"GitHub" => @github}
+      links: %{
+        "GitHub" => @github,
+        "Changelog" =>
+          "#{@github}/blob/#{@version}/CHANGELOG.md##{String.replace(@version, ".", "")}"
+      }
     ]
   end
 
   defp docs do
     [
+      main: "readme",
       source_url: @github,
-      extras: ["README.md", "CHANGELOG.md"],
+      source_ref: @version,
+      extras: [
+        "README.md",
+        "CHANGELOG.md": [title: "Changelog"],
+        "LICENSE.md": [title: "License (Apache-2.0)"],
+        "CONTRIBUTING.md": [title: "Contributing"],
+        "CODE_OF_CONDUCT.md": [title: "Code of Conduct"]
+      ],
       # api_reference: false,
       source_url_pattern: "#{@github}/blob/master/%{path}#L%{line}"
+    ]
+  end
+
+  defp aliases do
+    [
+      setup: ["deps.get"],
+      quality: [
+        "test",
+        "format --check-formatted",
+        # "credo",
+        "credo --mute-exit-status",
+        # mix deps.clean --unlock --unused
+        "deps.unlock --check-unused",
+        # mix deps.update
+        # "hex.outdated",
+        # "hex.audit",
+        "deps.audit",
+        "dialyzer --quiet-with-result"
+      ],
+      "quality.ci": [
+        "format --check-formatted",
+        "deps.unlock --check-unused",
+        # "hex.outdated",
+        "hex.audit",
+        "deps.audit",
+        "credo",
+        "dialyzer --quiet-with-result"
+      ]
     ]
   end
 end
